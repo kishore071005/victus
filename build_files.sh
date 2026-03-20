c00ef958393be360972ec50b67aa405d74f84df8
Nanda Kishore <kishore071005@gmail.com>
2026-03-21 02:00:00 +0530

    fix: cleanup build script and fix dependencies

--- build_files.sh
+++ build_files.sh
@@ -2,17 +2,14 @@
 
 echo "BUILD START"
 
-# Install python dependencies using the available pip
 python3 -m pip install --upgrade pip
 python3 -m pip install -r backend/requirements.txt
 
-# Build frontend
 cd frontend
-npm install --no-package-lock
+npm install --no-package-lock --legacy-peer-deps
 npm run build
 cd ..
 
-# Collect static files
 python3 backend/manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
